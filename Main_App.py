c25c235c84e8b3e256e0daa7b7bc204f7fb759eb
Noahking7 <16052@my.sanctamaria.school.nz>
2022-02-17 17:24:57 +1300

    update 3
    added 3 blog posts

--- Main_App.py
+++ Main_App.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
+posts = [
+
+    {
+        'author': 'Noah King',  
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Febuary 15, 2022'
+    },
+    
+    {
+        'author': 'Noah King',  
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Febuary 17, 2022'
+    },   
+
+    {
+        'author': 'Noah King',  
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'Febuary 18, 2022'
+    }
+
+]
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
